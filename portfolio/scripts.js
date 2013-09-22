2d11da817e6a0e113df983c4bfc597c14834f00c
Sandra Vilaro <sandravilaro@savicas.com>
2013-09-22 15:12:11 -0700

    adding a portfolio and an API work with Gmaps and BART

--- portfolio/scripts.js
+++ portfolio/scripts.js
@@ -1,40 +1,27 @@
-$(document).ready(function() {
+$(document).ready( function() {
 
-	$('section a').on('mouseenter', function(e){
+	$('.project').on('mouseenter', function(e){
 		e.preventDefault();
 		e.stopPropagation();
-		var project = $(this).val()
-    	,	object = [
-		{
-			object: "This is the <a href='http://savicas.github.io/googleHomePage/'> Google Home Page <a/>replica. ",
-			programming : ["HTML, CSS"],
-		},
-		{
-			object: "This is the <a href='http://savicas.github.io/37signals/'> 37 Signals Home Page <a/>replica. ",
-			programming : ["HTML, CSS", "jQuery"],
-		},
-				{
-			object: "This is a <a href='http://savicas.github.io/hotCold/'> Hot and Cold game <a/>. ",
-			programming : ["HTML, CSS", "jQuery", "JavaScript"],
-		},
-				{
-			object: "This is a <'http://savicas.github.io/shoppingList/'> Shopping List App <a/>. ",
-			programming : ["HTML, CSS","jQuery", "JavaScript"],
-		},
-				{
-			object: "This is a <a href='http://savicas.github.io/quizzApp/'> Quizz App <a/>. ",
-			programming : ["HTML, CSS","jQuery", "JavaScript"],
-		},
-				{
-			object: "This is a project with <a href='#'> APIs of Google Maps and BART.org <a/>. ",
-			programming : ["HTML, CSS","jQuery", "JavaScript"],
-		},
-	];
 
-		if (project === true) {
-			// $(this).toggleClass(".projectCurtain");
-
-		}
-
+		var img = $(this).find('img')
+			, curtain = $(this).find('.curtain')
+			, position = img.position()  		// we want to position the curtain in the same position than the current image
+			, margin = img.css('margin')    // we get the margin of the image
+																			// and apply it as the margin of the curtain, so the image and the curtain occupy the same space
+		
+		curtain.css('left', position.left)
+		       .css('top', position.top)
+		       .css('margin', margin)
+		       .css('position', 'absolute')
+					 .fadeIn();
 	});
+
+	$('.project').on('mouseleave', function(e){
+		e.preventDefault();
+		e.stopPropagation();
+
+		$(this).find('.curtain').fadeOut();
+	});
+
 });
